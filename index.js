8c9eced6541b6ff36919a8f321811def5661bae8
Alan Vinicius <alan123vinicius@gmail.com>
2020-08-03 12:51:39 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+  <script>
+
+    function exibir(param){
+
+        document.write(param);
+
+    }
+
+    function function_array(){
+        
+        nums = [
+            100
+        ]
+       
+        return (nums);
+    }
+
+    function find_max(array_param){
+       
+        let max_num = Number.NEGATIVE_INFINITY; 
+        
+        nums = [
+            100
+        ]
+        
+        for (let num of function_array(array_param)){
+            if (num > max_num){
+                // num = max_num -infinity
+                // max_num += num -infinity
+                // max_num = num 100
+                max_num += num;
+            }
+        }
+        
+        return exibir(max_num);
+
+        }
+    </script>
